10dcd7e69f79e08313912f80fc7fe334d4f49fa9
Pranali200 <hemanepranali08@gmail.com>
2024-12-12 17:13:03 +0530

    Slider job done

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,19 @@
 import Header from "./components/Header/Header";
+import Hero from "./components/Hero/Hero";
+import './App.css'
+import Residencies from "./components/Residencies/Residencies";
+import Companies from "./components/companies/companies";
 function App() {
   return (
     <div className = "App">
-      <Header/>
+      <div>  
+        <div className ="white-gradient"/>  
+         <Header/>
+         
+         <Hero/>
+    </div>
+    <Companies/>
+    <Residencies/>
     </div>
     );
 }
--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -1,24 +1,73 @@
 import React from 'react'
 import "./Hero.css"
+import {HiLocationMarker} from 'react-icons/hi'
+import CountUp from "react-countup";
 
 const Hero =()=>{
     return (
         <section className ="hero-wrapper">
-            <div className ="padding innerWidth flexCenter hero-cointainer">
+            <div className ="padding innerWidth flexCenter hero-container">
+                 
+                 {/*left section*/}
+                <div className ="flexColStart hero-left">
+                    <div clasName="hero-title">
+                        <div className ="orange-circle"/>
+                        <h1>
+                            Discover<br/>
+                            Most Suitable<br/> Property 
+                        </h1>
+                    </div>
 
-                <div className ="hero-left">
-                    Left section
+                    <div className="flexColStart hero-des">
+                        <span className ="secondaryText">Find properties in a seconds that good to you .</span>
+                        <span className ="secondaryText">Now, find a residence in a click!</span>
+                    </div>
+                   
+
+                   <div className =" flexCenter search-bar">
+                    <HiLocationMarker color ="var(--blue)" size={25}/>
+                     <input type="text" />
+                     <button className ="button" >Search</button>
+                   </div>
+
+                 <div className ="flexCenter stats">
+                    <div className ="flexColCenter stat">
+                        <span>
+                            <CountUp start ={8000} end={9000} duration={4}/>
+                            <span>+</span>
+                        </span>
+                        <span className ="secondaryText">Premium Products</span>
+                    </div>
+                    
+                    <div className =" flexColCenter stat">
+                        <span>
+                            <CountUp start ={1950} end={2000} duration={4}/>
+                            <span>+</span>
+                        </span>
+                        <span className ="secondaryText">Happy Clients</span>
+                    </div>
+                   
+                  
+                    <div className ="flexColCenter stat">
+                        <span>
+                            <CountUp  end={28} />
+                            <span>+</span>
+                        </span>
+                        <span className ="secondaryText">Award Winning</span>
+                    </div>
+                 </div>
                 </div>
-
+               
+               {/*right section*/}
                 <div className ="flexCenter hero-right">
-                    <div className ="image-cointainer">
+                    <div className ="image-container">
                         <img src ="./hero-image.png" alt=""/>
 
                     </div>
                 </div>
-            </div>
+             </div>
         </section>
-    )
-}
+    );
+};
 
 export default Hero
